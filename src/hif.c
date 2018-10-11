a54c85aa1638562db0c35624f9bcb0f99969f4a6
Jason Short <jgshort@openiep.com>
2018-10-10 21:27:37 -0700

    Move export-to-json logic to storage_adapter
    - Rename hif_feel to hif_command to reflect new behavior
    - Move json export from hif.c to storage_adapter.c

--- src/hif.c
+++ src/hif.c
@@ -56,80 +56,18 @@ static char * str_lower(char * s) {
 	return s;
 }
 
-static int query_command(sqlite3 * db, void * payload) {
-	sqlite3_stmt * stmt = NULL;
-	char * sql = "select * from hif_feels;";
-		
-	int count = 10; //query_table_row_count(db, "hif_feels");
-	if(count < 0) { exit(-1); }
-	
-	fprintf(stdout, "{\n");
-	fprintf(stdout, "\t\"feels\": [");
-	if(count > 0) fprintf(stdout, "\n");
-	int rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
-	if(rc != SQLITE_OK) goto err2;
-
-	int i = 0;
-	int col_count = sqlite3_column_count(stmt);
-
-	while(sqlite3_step(stmt) == SQLITE_ROW) {
-		int col = 0;
-
-		fprintf(stdout, "\t\t{ ");
-		while(col < col_count) {
-			const char * data = NULL;
-			const int feel_col = 1;
-
-			data = (const char *)sqlite3_column_name(stmt, col);
-			fprintf(stdout, "\"%s\": ", data);
-			data = (const char *)sqlite3_column_text(stmt, col);
-			if(col < 1) {
-				fprintf(stdout, "%s", data);
-			} else {
-				if(col == feel_col) {
-					if(data[0] == '0') {
-						fprintf(stdout, "\"bad\"");
-					} else if(data[0] == '1') {
-						fprintf(stdout, "\"meh\"");
-					} else if(data[0] == '2') {
-						fprintf(stdout, "\"woo\"");
-					}
-				} else {
-					fprintf(stdout, "\"%s\"", data);
-				}
-			}
-			if(col < col_count - 1) fprintf(stdout, ", ");
-			col++;
-		}
-		i++;
-		if(i < count) fprintf(stdout, " },\n");
-		else fprintf(stdout, " }\n");
-
-	}
-
-	sqlite3_finalize(stmt);
-	if(count > 0) fprintf(stdout, "\t");
-	fprintf(stdout, "]\n");
-	fprintf(stdout, "}\n");
-
-err2:
-err1:
-err0:
-	return rc;
-}
-
-static hif_feel str_to_command(const char * s) {
-	hif_feel feel;
+static hif_command str_to_command(const char * s) {
+	hif_command feel;
 	if(strncmp(s, "add", sizeof("add") - 1) == 0) {
-		return HIF_COMMAND | HIF_COMMAND_ADD;
+		return HIF_COMMAND | HIF_COMMAND_ADD_FEEL;
 	} else if(strncmp(s, "json", sizeof("json") - 1) == 0) {
 		return HIF_COMMAND | HIF_COMMAND_JSON;
 	} else if(strncmp(s, "delete", sizeof("delete") - 1) == 0) {
-		return HIF_COMMAND | HIF_COMMAND_DELETE;
+		return HIF_COMMAND | HIF_COMMAND_DELETE_FEEL;
 	} else if(strncmp(s, "count", sizeof("count") - 1) == 0) {
 		return HIF_COMMAND | HIF_COMMAND_COUNT;
 	} else if(strncmp(s, "create-context", sizeof("create-context") - 1) == 0) {
-		return HIF_COMMAND | HIF_COMMAND_INIT;
+		return HIF_COMMAND | HIF_COMMAND_CREATE;
 	} else if(strncmp(s, "create-feel", sizeof("create-feel") - 1) == 0) {
 		return HIF_COMMAND | HIF_COMMAND_CREATE_FEEL;
 	} else if(strncmp(s, "help", sizeof("help") - 1) == 0) {
@@ -174,7 +112,7 @@ int main(int argc, char **argv) {
 
 	sqlite3_initialize();
 
-	hif_feel command = str_to_command(p);
+	hif_command command = str_to_command(p);
 	
 	storage_adapter const * adapter = storage_adapter_alloc();
 	if(!context_exists("hif.db")) {
@@ -182,7 +120,7 @@ int main(int argc, char **argv) {
 	}
 	adapter->open_storage(adapter, "hif.db");
 
-	if(command & HIF_COMMAND_INIT) {
+	if(command & HIF_COMMAND_CREATE) {
 		const char * path = NULL;
 		if(argc >= 3) {
 			path = argv[2];
@@ -190,7 +128,7 @@ int main(int argc, char **argv) {
 		adapter->create_storage(adapter, path);
 		fprintf(stdout, "Created context %s\n", path);
 	}
-	else if(command & HIF_COMMAND_ADD) {
+	else if(command & HIF_COMMAND_ADD_FEEL) {
 		char * description = NULL;
 		adapter->insert_feel(adapter, argv[2], &description);
 		fprintf(stdout, "Feels added %s\n", description ? description : ":)");
@@ -200,9 +138,9 @@ int main(int argc, char **argv) {
 		fprintf(stdout, "%i\n", count);
 	}
 	else if(command & HIF_COMMAND_JSON) {
-		/* rc = ommand.command(db, NULL); */
+		adapter->export(adapter, NULL);
 	}
-	else if(command & HIF_COMMAND_DELETE) {
+	else if(command & HIF_COMMAND_DELETE_FEEL) {
 		if(argc < 3) {
 			print_help(stderr);
 			goto err1;
