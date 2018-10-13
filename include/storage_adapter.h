2e90534bbc78e21a1534964fa95da312193280a8
Jason Short <jgshort@openiep.com>
2018-10-13 11:32:59 -0700

    Improve error handling in main
    - Update storage_adapter close to return int
    - Refactor main code to handle appropriate error code(s)

--- include/storage_adapter.h
+++ include/storage_adapter.h
@@ -12,7 +12,7 @@ void storage_adapter_free(storage_adapter * adapter);
 typedef struct storage_adapter {
 	int (*create_storage)(storage_adapter const * adapter, char const * path);
 	int (*open_storage)(storage_adapter const * adapter, char const * context_name);
-	void (*close)(storage_adapter const *adapter);
+	int (*close)(storage_adapter const *adapter);
 	
 	int (*create_feel)(storage_adapter const * adapter, char const * feel, char const * description);
 	int (*insert_feel)(storage_adapter const * adapter, char const * feel, char **description);
