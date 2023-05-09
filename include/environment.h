82c4bf34082f394910f8a56e530721e647577457
Jason Short <53501+jgshort@users.noreply.github.com>
2023-05-08 17:33:08 -0700

    Add custom version of asprintf
    - Not sure if this is the way to do it, but add an asprintf function for
      environments that don't support it

--- include/environment.h
+++ include/environment.h
@@ -1,11 +1,17 @@
 #ifndef HIF_ENVIRONMENT
 #define HIF_ENVIRONMENT
 
+#include <stdio.h>
+
 char const * get_user_home();
 char const * get_config_path();
 void ensure_config_path();
 char * alloc_concat_path(char const * root_path, char const * path);
 
 int context_exists(char const * context_name);
+
+#ifndef asprintf
+int asprintf(char **ret, const char *format, ...);
+#endif
 
 #endif /* HIF_ENVIRONMENT */
